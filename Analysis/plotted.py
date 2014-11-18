f2329bd3541920d9d4b3a2b9b8ca91ba874cd947
Jonathan Dray <jonathan.dray@yahoo.co.uk>
2014-11-18 23:10:18 +0000

    New Plotted Created

---
+++ Analysis/plotted.py
@@ -0,0 +1,36 @@
+__author__ = 'Jon'
+import matplotlib as mpl
+
+mpl.use('Agg') # Must be used before pyplot import to use Agg
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# define x
+
+x = np.arange(1,10,0.01)
+
+#plot points with legend labels
+
+plt.plot(x,[xi**2 for xi in x],label='Power 2')
+plt.plot(x,[xi**3 for xi in x],label='Power 3')
+plt.plot(x,[xi**3.2 for xi in x],label='Power 3.2')
+
+#grid
+
+plt.grid(True)
+
+#labels
+
+plt.xlabel('Samples')
+plt.ylabel('Values')
+plt.title('Simple Plot')
+plt.legend(loc='best')
+
+# save chart
+
+#plt.savefig('plot1.png',dpi=200, transparent=True)
+
+# show chart
+
+plt.show()
